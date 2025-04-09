d327c56f990bea7cc9ce81dfe92bb771323159c0
Nazar <nazargorodyko@gmail.com>
2025-04-09 12:42:05 +0300

    Delete else

--- cinema/views.py
+++ cinema/views.py
@@ -31,6 +31,5 @@ def movie_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    else:
-        movie.delete()
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    movie.delete()
+    return Response(status=status.HTTP_404_NOT_FOUND)
